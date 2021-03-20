f285c652f8360ab3429e8c0f18ae5cacba666748
exqudens <exqudens@gmail.com>
2021-03-19 20:43:58 -0700

    step 18

--- extension.cmake
+++ extension.cmake
@@ -9,6 +9,78 @@ elseif("file_download" STREQUAL "${CMAKE_ARGV3}")
     file(DOWNLOAD ${CMAKE_ARGV4} ${CMAKE_ARGV5})
 endif()
 
+# declare macro 'add_custom_target_install'
+macro(add_custom_target_install
+    name
+    depends
+    input
+    output
+    command
+)
+    add_custom_command(
+        OUTPUT  ${output}
+        COMMAND ${command}
+        ARGS    -E
+                make_directory
+                ${output}
+        COMMAND ${command}
+        ARGS    -E
+                copy_directory
+                ${input}
+                ${output}
+        COMMENT "Installing project."
+    )
+    add_custom_target(${name}
+        DEPENDS ${output}
+    )
+    add_dependencies(${name} ${depends})
+endmacro()
+
+# declare macro 'add_custom_target_pack'
+macro(add_custom_target_pack
+    name
+    depends
+    input
+    output
+    command
+)
+    add_custom_target(${name}
+        COMMAND           ${command}
+                          -E
+                          tar
+                          cfv
+                          ${output}
+                          --format=zip
+                          --
+                          .
+        BYPRODUCTS        ${output}
+        WORKING_DIRECTORY ${input}
+        COMMENT           "Packaging project."
+    )
+    add_dependencies(${name} ${depends})
+endmacro()
+
+# declare macro 'add_custom_target_upload'
+macro(add_custom_target_upload
+    name
+    depends
+    input
+    output
+    command
+    script
+)
+    add_custom_target(${name}
+        COMMAND ${command}
+                -P
+                ${script}
+                file_upload
+                ${input}
+                ${output}
+        COMMENT "Uploading project."
+    )
+    add_dependencies(${name} ${depends})
+endmacro()
+
 # declare function 'set_if_not_defined'
 function(set_if_not_defined variableName)
     if(NOT DEFINED ${variableName})
